b5136749bb51221456dd66c0f3876ff3a5f098db
Andrea Musso <musso2a@gmail.com>
2022-06-27 13:41:14 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return response()->json(['message' => "Welcome to api"], 200);
+    return view('welcome');
+   // return response()->json(['message' => "Welcome to api"], 200);
 });
